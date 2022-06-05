2ebcd32e2217e5242436b4198c4e7b0741f5bfd9
Aleksander Prymek <prymeka@tcd.ie>
2022-06-05 17:08:02 +0100

    added selection comparison

--- script.js
+++ script.js
@@ -16,11 +16,22 @@ scissorsButton.addEventListener('click', e => {
 
 function runGame(playerSelection) {
     // select random card for the computer
-    const possibleSelections = ['rock', 'paper', 'scissors']
+    const possibleSelections = ['rock', 'paper', 'scissors'];
     const computerSelection = possibleSelections[Math.floor(Math.random()*3)];
+
+    const playerWon = (
+        (playerSelection === 'rock' && computerSelection === 'scissors') 
+    || (playerSelection === 'paper' && computerSelection === 'rock') 
+    || (playerSelection === 'scissors' && computerSelection === 'paper')
+    )
+
+    console.log(`Player: ${playerSelection}; Computer: ${computerSelection}.`);
+
     if (playerSelection === computerSelection) {
-        console.log('DRAW')
+        console.log('DRAW');
+    } else if (playerWon === true) {
+        console.log('WIN');
+    } else {
+        console.log('LOSE')
     }
-    console.log(playerSelection)
-    console.log(computerSelection)
 }
